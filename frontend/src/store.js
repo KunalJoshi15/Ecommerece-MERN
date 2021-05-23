bb64a6d2cfeec1b9bec9f349bc49fa866f8a8502
Your Name <kjoshi087@gmail.com>
2021-05-23 22:46:45 +0530

    Added the review system in the program

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { createStore,combineReducers,applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListsReducer,productUpdateReducer,productDetailsReducer, productDeleteReducer,productCreateReducer } from './reducers/productReducers'
+import { productListsReducer,productReviewCreateReducer,productUpdateReducer,productDetailsReducer, productDeleteReducer,productCreateReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { userDetailsReducer, userLoginReducer, userRegisterReducer,userUpdateProfileReducer,userListReducer,userDeleteReducer, userUpdateReducer } from './reducers/userReducers'
 import { orderCreateReducer,orderDetailsReducer,orderListReducer,orderPayReducer,orderListMyReducer, orderDeliverReducer } from './reducers/orderReducers'
@@ -12,6 +12,7 @@ const reducer = combineReducers({
     productDelete:productDeleteReducer,
     productCreate: productCreateReducer,
     productUpdate: productUpdateReducer,
+    productReviewCreate:productReviewCreateReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
